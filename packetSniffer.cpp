c67d933432b086585a6ce70b6cb433b0988233bc
Christopher Valle <christxpherv@gmail.com>
2024-03-11 17:02:16 -0500

    added conditional to check if we have a program name and device name, if so, set the dev variable to equal the device name

--- packetSniffer.cpp
+++ packetSniffer.cpp
@@ -12,3 +12,23 @@
 #ifdef __APPLE__
 #define MACOS
 #endif
+
+int main(int argc, char* argv[]) {
+
+    
+    char* dev;
+
+    /* check if there are at least two arguments (program name and one additional argument) */
+    if (argc >= 2) {
+        /* 
+         * we will access command line arguments below using the
+         * argv array which holds argument separated by spaces 
+         */
+        std::cout << "Program name : " << argv[0] << std::endl;
+        std::cout << "Device Name  : " << argv[1] << std::endl;
+        
+        /* we assign the device name to the device variable */
+        dev = argv[1];
+        
+    return 0;
+}
